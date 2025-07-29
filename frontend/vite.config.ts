7a31b0a36ddf4d82f706b895d15b2a74fa9691b1
Le Hoang Lam <lhlam.lehoang@gmail.com>
2025-07-29 08:58:12 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['https://bigbikeblitz-production.up.railway.app'],
     host: '0.0.0.0',
     port: 5173,
     proxy: {
